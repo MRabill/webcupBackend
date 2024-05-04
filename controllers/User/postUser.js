8b65427a50ed29c773e1c8a88bff4339533b8d17
Edoo Junaid <edooj511@gmail.com>
2024-05-04 17:20:05 +0400

    added unique contraint and return OTP

--- controllers/User/postUser.js
+++ controllers/User/postUser.js
@@ -1,22 +1,42 @@
-// const { router, knexDb } = require("../../utils/routes.imports.utils");
 const { router, knexDb } = require("../../utils/routes.imports.utils");
 
 router.post("/APIBACKEND/post-user", async (req, res) => {
   try {
-    const { username,password } = req.body;
+    const { username, password } = req.body;
+    
+    // Generate a random 4-digit number for OTP
+    const otp = Math.floor(1000 + Math.random() * 9000);
+    
+    // Insert the user into the database
     await knexDb("User").insert({
       username: username,
-      password:password
+      password: password
     });
 
     res.status(200).send({
-      payload: [],
+      payload: {
+        otp: otp,
+      },
       message: "Successfully add user",
       success: true,
     });
   } catch (e) {
-    console.error({ error: e });
-    res.status(500).send([]);
+
+    // Check if the error is due to duplicate entry for username
+    if (e.code === 'ER_DUP_ENTRY') {
+      res.status(400).send({
+        payload: null,
+        message: "Username already exists",
+        success: false,
+      });
+    } else {
+      console.error({ error: e });
+      res.status(500).send({
+        payload: null,
+        message: "Internal server error",
+        success: false,
+      });
+    }
   }
 });
 
